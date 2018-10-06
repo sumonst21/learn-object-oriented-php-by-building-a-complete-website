3dd99f162f49659380a7463c54f20a745d587ff3
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-06 22:26:40 +0600

    Update functions-conditionals.php

---
+++ functions-conditionals.php
@@ -0,0 +1,26 @@
+<?php
+    // Very basic functions
+    /*
+    function greet(){
+        echo 'Hello World';
+    }
+
+    function greet($name){
+        echo 'Hello '. $name;
+    }
+
+    function greet($greeting, $name='John'){
+        echo strtoupper($greeting. ' '. $name);
+    }
+    greet('Whats Up', 'Sumon');
+    */
+
+    // Conditionals
+    $num = 10;
+
+    if ($num == 10) {
+        echo 'Correct';
+    }
+
+
+?>
