7356b8bd43a39af9649246a4190ed80f27b9145b
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-06 23:23:54 +0600

    Update OOP Fundamentals/Classes and Methods.php

--- OOP Fundamentals/Classes and Methods.php
+++ OOP Fundamentals/Classes and Methods.php
@@ -1,5 +1,11 @@
 <?php
 class User{
+    // classes & methods is the constructor
+    public function __construct()
+    {
+        echo 'Constructor Called';
+    }
+
     // access identifier and methods
     public function register(){
         echo 'User Registered';
@@ -8,10 +14,16 @@ class User{
     public function login($username, $password){
         echo $username. ' is now logged in';
     }
+
+    // destructor 
+    public function __destruct()    // use can use this destructor to close database connection or anything at the end of your code
+    {
+        echo 'Destructor Called';
+    }
 }
 // Instantiate the class
 $User = new User;
 
-//$User->register();
-$User->login('Sumon', '1234');
+$User->register();
+//$User->login('Sumon', '1234');
 ?>
