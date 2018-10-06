8d17f791da059b63029325b613fb8a8ab7241337
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-07 00:06:22 +0600

    Update OOP Fundamentals/__get and __set Magic Methods.php

--- OOP Fundamentals/__get and __set Magic Methods.php
+++ OOP Fundamentals/__get and __set Magic Methods.php
@@ -1,2 +1,23 @@
 <?php
-?>
+class Post{
+    private $name;
+    // __set - the megic method
+    public function __set($name, $value){
+        echo 'Setting '. $name.' to <strong>'.$value.'</strong><br />';
+        $this->name = $value;
+    }
+    // __get - magic method
+    public function __get($name){
+        echo 'Getting ' . $name . '  <strong>' . $this->name . '</strong><br />';
+    }
+    // __isset - megic method
+    public function __isset($name){
+        echo 'Is '.$name.' set?<br />';
+        return isset($this->name);
+    }
+}
+
+$post = new Post;
+$post->name = 'Testing';
+echo $post->name;
+var_dump(isset($post->name));
