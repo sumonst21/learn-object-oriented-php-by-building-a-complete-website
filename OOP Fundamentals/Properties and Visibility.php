506097f0df657706bd2a330f457feee9e9ea0b1f
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-06 23:53:48 +0600

    Update OOP Fundamentals/Properties and Visibility.php

--- OOP Fundamentals/Properties and Visibility.php
+++ OOP Fundamentals/Properties and Visibility.php
@@ -5,15 +5,17 @@ class User
      * before to get to access modifier lets just add a couples properties here
      * we can access properties in all of our methods
      */
-    public $id = 33; // we can set default value for properties like: public $id = 1;
-    public $username;
-    public $email;
-    public $password;
+    private $id = 33; // we can set default value for properties like: public $id = 1;
+    private $username;
+    private $email;
+    private $password;
 
     // classes & methods is the constructor
-    public function __construct()
+    public function __construct($username, $password)
     {
         //echo 'Constructor Called <br />';
+        $this->username = $username;
+        $this->password = $password;
     }
 
     // access identifier and methods
@@ -22,19 +24,21 @@ class User
         echo 'User Registered';
     }
 
-    public function login($username, $password)
+    public function login()
     {
         //echo $username. ' is now logged in';
-
+        //$this->username = $username; // we are gonnna use the above property: $username here
+        //$this->password = $password;
         // we want to call authuser from within login so
-        $this->auth_user($username, $password);
+        $this->auth_user(); // we don't need to pass any arguments here because we know the class properties above this  line
 
     }
 
-    public function auth_user($username, $password)
+    public function auth_user()
     {
         //echo $username . ' is authenticated<br/>';
-        echo $this->id;
+        //echo $this->id;
+        //echo $this->username. ' is authenticated';
     }
 
     // destructor 
@@ -44,8 +48,11 @@ class User
     }
 }
 // Instantiate the class
-$User = new User;
+$User = new User('Sumon', '1234');
+
+//echo $User->username;
 
 //$User->register();
-$User->login('Sumon', '1234');
+//$User->login('Sumon', '1234');
+//$User->login();
 ?>
