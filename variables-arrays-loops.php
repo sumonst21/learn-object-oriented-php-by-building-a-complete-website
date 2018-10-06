a5c1a91b68124a1c13ce4d60428004595b9655fc
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-06 22:26:12 +0600

    Update variables-arrays-loops.php

---
+++ variables-arrays-loops.php
@@ -0,0 +1,56 @@
+<?php
+    //echo 'It WORKS!';
+
+    /*
+ * Arrays 
+ */
+    /*
+    $numbers = array(12,45,22,34,65);
+    $numbers = [12,45,22,34,65];
+     //print_r($numbers);
+
+    //echo $numbers[4]
+
+    $ages = array(
+        'John' => 35,
+        'Mary' => 27,
+        'Bob' => 55 
+    );
+
+   // echo $ages['Mary'];
+
+   //array_pop($ages); // array_pop : pops out the last value from array
+   //array_shift($ages); // array_shift : does the oposit of array_pop - thhis removes from the begining of values
+
+   //print_r($ages);
+
+   /*
+ * loops
+ */ 
+    /*
+    for($i = 0;$i <= 5;$i++){
+        echo $i;
+    }
+    
+    
+    $a = 0;
+    while($a <= 10) {
+      echo 'Number '.$a.'<br />';  # code...
+      $a++;
+    }
+ */
+
+$ages = array(
+    'John' => 35,
+    'Mary' => 27,
+    'Bob' => 55
+);
+
+$numbers = array(12, 45, 22, 34, 65);
+
+foreach ($ages as $name => $age) {
+    echo $name. ' is '.$age.' years old<br />';
+}
+
+
+?>
