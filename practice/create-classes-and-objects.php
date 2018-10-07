4ed26ee34a98eaf5485362abe83b5f1620201a30
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-08 00:37:08 +0600

    Update practice/create-classes-and-objects.php

--- practice/create-classes-and-objects.php
+++ practice/create-classes-and-objects.php
@@ -102,3 +102,54 @@ echo $hello . ",  " . $user1->firstName . " " . $user1->lastName;
  
 echo $hello . ",  " . $user2->firstName . " " . $user2->lastName;
 
+
+
+/*
+ * Here is the full code that we have written during this tutorial:
+ */
+
+// Declare the class
+class Car {
+ 
+  // properties
+  public $comp; 
+  public $color = 'beige';
+  public $hasSunRoof = true;
+ 
+   // method that says hello
+  public function hello()
+  {
+    return "beep";
+  }
+}
+ 
+// Create an instance
+$bmw = new Car ();
+$mercedes = new Car ();
+ 
+// Get the values
+echo $bmw -> color; // beige
+echo "<br />";
+echo $mercedes -> color; // beige
+echo "<hr />";
+ 
+// Set the values
+$bmw -> color = 'blue';
+$bmw -> comp = "BMW";
+$mercedes -> comp = "Mercedes Benz";
+ 
+// Get the values again
+echo $bmw -> color; // blue
+echo "<br />";
+echo $mercedes -> color; // beige
+echo "<br />";
+echo $bmw -> comp; // BMW
+echo "<br />";
+echo $mercedes -> comp; // Mercedes Benz
+echo "<hr />";
+ 
+// Use the methods to get a beep
+echo $bmw -> hello(); // beep
+echo "<br />";
+echo $mercedes -> hello(); // beep
+
