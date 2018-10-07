3c6da60c73bed1a17a09d4f2d8f94a09d9315596
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-07 22:39:51 +0600

    Updated coding standard

--- Introduction-to-PHP/functions-conditionals.php
+++ Introduction-to-PHP/functions-conditionals.php
@@ -1,41 +1,44 @@
 <?php
-    // Very basic functions
-    /*
-    function greet(){
-        echo 'Hello World';
-    }
+/**
+ * Very basic functions
+ */
+function greet()
+{
+    echo 'Hello World';
+}
+/*
+function greet($name)
+{
+    echo 'Hello '. $name;
+}
 
-    function greet($name){
-        echo 'Hello '. $name;
-    }
+function greet($greeting, $name='John'){
+    echo strtoupper($greeting. ' '. $name);
+}
+greet('Whats Up', 'Sumon');
+ */
 
-    function greet($greeting, $name='John'){
-        echo strtoupper($greeting. ' '. $name);
-    }
-    greet('Whats Up', 'Sumon');
+// Conditionals
 
-    // Conditionals
-    /*
-    $num1 = 50;
-    $num2 = 500;
+$num1 = 50;
+$num2 = 500;
 
-    if ($num1 == 40) {
-        echo 'Correct';
-    } elseif($num1 == 50) {
-        echo 'Correct';
-    } else {
-        echo 'Wrong';
-    }
-    */
-
-$num1 = 30;
-$num2 = 20;
-
-if($num1 == 30 && $num2 == 20){ # *||* this is OR and this *&&* this is AND conditions
+if ($num1 == 40) {
+    echo 'Correct';
+} elseif ($num1 == 50) {
     echo 'Correct';
 } else {
     echo 'Wrong';
 }
 
+$num1 = 30;
+$num2 = 20;
 
-?>
+/** 
+ * || this is OR and this *&&* this is AND conditions 
+ */
+if ($num1 == 30 && $num2 == 20) {
+    echo 'Correct';
+} else {
+    echo 'Wrong';
+}
