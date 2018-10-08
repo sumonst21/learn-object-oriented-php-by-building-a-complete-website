592ba3489cb854f5da04abe8d54ffabeb9b7a4f6
Md. Sumon Islam <developer.sumon@gmail.com>
2018-10-09 02:30:37 +0600

    Update Advanced-OOP/Object-Iteration.php

--- Advanced-OOP/Object-Iteration.php
+++ Advanced-OOP/Object-Iteration.php
@@ -4,4 +4,29 @@
  * Definitions of iteration
  * noun: the repetition of a process or utterance.
  */
+class People{
+	// public properties
+	public $person1 = 'Mike';
+	public $person2 = 'Shelly';
+	public $person3 = 'Jeff';
 
+	// protected properties
+	protected $person4 = 'john';
+
+	// private properties
+	private $person5 = 'Jen';
+
+	// method
+	function iterateObject(){
+		// foreach loop
+		foreach ($this as $key => $value) {
+			print "$key => $value\n";
+		}
+	}
+}
+
+// instantiate class people
+$people = new People;
+
+// call iterateObject
+$people->iterateObject();
